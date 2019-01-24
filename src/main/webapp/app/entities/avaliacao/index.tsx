cf8e116dc4b4e1397254f48d2a8e5dbb633d5471
Léo <leonardo.pimentel@ninebee.it>
2019-01-24 16:45:20 -0200

    Removendo código não usado.

--- src/main/webapp/app/entities/avaliacao/index.tsx
+++ src/main/webapp/app/entities/avaliacao/index.tsx
@@ -5,14 +5,11 @@ import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
 import Avaliacao from './avaliacao';
 import AvaliacaoDetail from './avaliacao-detail';
-import AvaliacaoUpdate from './avaliacao-update';
 import AvaliacaoDeleteDialog from './avaliacao-delete-dialog';
 
 const Routes = ({ match }) => (
   <>
     <Switch>
-      <ErrorBoundaryRoute exact path={`${match.url}/new`} component={AvaliacaoUpdate} />
-      <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={AvaliacaoUpdate} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={AvaliacaoDetail} />
       <ErrorBoundaryRoute path={match.url} component={Avaliacao} />
     </Switch>
--- src/test/javascript/e2e/entities/avaliacao/avaliacao.spec.ts
+++ src/test/javascript/e2e/entities/avaliacao/avaliacao.spec.ts
@@ -4,8 +4,6 @@ import { browser, element, by, protractor } from 'protractor';
 import NavBarPage from './../../page-objects/navbar-page';
 import SignInPage from './../../page-objects/signin-page';
 import AvaliacaoComponentsPage from './avaliacao.page-object';
-import { AvaliacaoDeleteDialog } from './avaliacao.page-object';
-import AvaliacaoUpdatePage from './avaliacao-update.page-object';
 import { waitUntilDisplayed, waitUntilHidden } from '../../util/utils';
 
 const expect = chai.expect;
@@ -13,9 +11,7 @@ const expect = chai.expect;
 describe('Avaliacao e2e test', () => {
   let navBarPage: NavBarPage;
   let signInPage: SignInPage;
-  let avaliacaoUpdatePage: AvaliacaoUpdatePage;
   let avaliacaoComponentsPage: AvaliacaoComponentsPage;
-  /*let avaliacaoDeleteDialog: AvaliacaoDeleteDialog;*/
 
   before(async () => {
     await browser.get('/');
@@ -36,98 +32,6 @@ describe('Avaliacao e2e test', () => {
     avaliacaoComponentsPage = new AvaliacaoComponentsPage();
     expect(await avaliacaoComponentsPage.getTitle().getText()).to.match(/Avaliacaos/);
   });
-
-  it('should load create Avaliacao page', async () => {
-    await avaliacaoComponentsPage.clickOnCreateButton();
-    avaliacaoUpdatePage = new AvaliacaoUpdatePage();
-    expect(await avaliacaoUpdatePage.getPageTitle().getAttribute('id')).to.match(/dcpdesconformidadesApp.avaliacao.home.createOrEditLabel/);
-  });
-
-  /* it('should create and save Avaliacaos', async () => {
-        const nbButtonsBeforeCreate = await avaliacaoComponentsPage.countDeleteButtons();
-
-        await avaliacaoUpdatePage.setIniciadaEmInput('01/01/2001' + protractor.Key.TAB + '02:30AM');
-        expect(await avaliacaoUpdatePage.getIniciadaEmInput()).to.contain('2001-01-01T02:30');
-        await avaliacaoUpdatePage.setLatitudeInicioAvaliacaoInput('5');
-        expect(await avaliacaoUpdatePage.getLatitudeInicioAvaliacaoInput()).to.eq('5');
-        await avaliacaoUpdatePage.setLongitudeInicioAvaliacaoInput('5');
-        expect(await avaliacaoUpdatePage.getLongitudeInicioAvaliacaoInput()).to.eq('5');
-        await avaliacaoUpdatePage.setNomeResponsavelLojaInput('nomeResponsavelLoja');
-        expect(await avaliacaoUpdatePage.getNomeResponsavelLojaInput()).to.match(/nomeResponsavelLoja/);
-        await avaliacaoUpdatePage.setProntuarioResponsavelLojaInput('5');
-        expect(await avaliacaoUpdatePage.getProntuarioResponsavelLojaInput()).to.eq('5');
-        await avaliacaoUpdatePage.setSubmetidoEmInput('01/01/2001' + protractor.Key.TAB + '02:30AM');
-        expect(await avaliacaoUpdatePage.getSubmetidoEmInput()).to.contain('2001-01-01T02:30');
-        await avaliacaoUpdatePage.setLatitudeSubmissaoAvaliacaoInput('5');
-        expect(await avaliacaoUpdatePage.getLatitudeSubmissaoAvaliacaoInput()).to.eq('5');
-        await avaliacaoUpdatePage.setLongitudeSubmissaoAvaliacaoInput('5');
-        expect(await avaliacaoUpdatePage.getLongitudeSubmissaoAvaliacaoInput()).to.eq('5');
-        await avaliacaoUpdatePage.setObservacaoSubmissaoEnviadaForaDaLojaInput('observacaoSubmissaoEnviadaForaDaLoja');
-        expect(await avaliacaoUpdatePage.getObservacaoSubmissaoEnviadaForaDaLojaInput()).to.match(/observacaoSubmissaoEnviadaForaDaLoja/);
-        await avaliacaoUpdatePage.statusSelectLastOption();
-        await avaliacaoUpdatePage.criticidadePainelSelectLastOption();
-        await avaliacaoUpdatePage.nivelEficienciaGeralSelectLastOption();
-        await avaliacaoUpdatePage.nivelEficienciaProcedimentoSelectLastOption();
-        await avaliacaoUpdatePage.nivelEficienciaPessoaSelectLastOption();
-        await avaliacaoUpdatePage.nivelEficienciaProcessoSelectLastOption();
-        await avaliacaoUpdatePage.nivelEficienciaProdutoSelectLastOption();
-        await avaliacaoUpdatePage.setCanceladoEmInput('01/01/2001' + protractor.Key.TAB + '02:30AM');
-        expect(await avaliacaoUpdatePage.getCanceladoEmInput()).to.contain('2001-01-01T02:30');
-        await avaliacaoUpdatePage.setMotivoCancelamentoInput('motivoCancelamento');
-        expect(await avaliacaoUpdatePage.getMotivoCancelamentoInput()).to.match(/motivoCancelamento/);
-        await avaliacaoUpdatePage.setPercentualPerdaInput('5');
-        expect(await avaliacaoUpdatePage.getPercentualPerdaInput()).to.eq('5');
-        await avaliacaoUpdatePage.setFinanceiroPerdaInput('5');
-        expect(await avaliacaoUpdatePage.getFinanceiroPerdaInput()).to.eq('5');
-        await avaliacaoUpdatePage.setPontuacaoPerdaInput('5');
-        expect(await avaliacaoUpdatePage.getPontuacaoPerdaInput()).to.eq('5');
-        await avaliacaoUpdatePage.statusPerdaSelectLastOption();
-        await avaliacaoUpdatePage.categorizacaoPerdaSelectLastOption();
-        await avaliacaoUpdatePage.setPercentualQuebraInput('5');
-        expect(await avaliacaoUpdatePage.getPercentualQuebraInput()).to.eq('5');
-        await avaliacaoUpdatePage.setFinanceiroQuebraInput('5');
-        expect(await avaliacaoUpdatePage.getFinanceiroQuebraInput()).to.eq('5');
-        await avaliacaoUpdatePage.setPontuacaoQuebraInput('5');
-        expect(await avaliacaoUpdatePage.getPontuacaoQuebraInput()).to.eq('5');
-        await avaliacaoUpdatePage.statusQuebraSelectLastOption();
-        await avaliacaoUpdatePage.categorizacaoQuebraSelectLastOption();
-        const selectedImportadoViaPlanilha = await avaliacaoUpdatePage.getImportadoViaPlanilhaInput().isSelected();
-        if (selectedImportadoViaPlanilha) {
-            await avaliacaoUpdatePage.getImportadoViaPlanilhaInput().click();
-            expect(await avaliacaoUpdatePage.getImportadoViaPlanilhaInput().isSelected()).to.be.false;
-        } else {
-            await avaliacaoUpdatePage.getImportadoViaPlanilhaInput().click();
-            expect(await avaliacaoUpdatePage.getImportadoViaPlanilhaInput().isSelected()).to.be.true;
-        }
-        await avaliacaoUpdatePage.setCaminhoArquivoPlanilhaInput('caminhoArquivoPlanilha');
-        expect(await avaliacaoUpdatePage.getCaminhoArquivoPlanilhaInput()).to.match(/caminhoArquivoPlanilha/);
-        await avaliacaoUpdatePage.avaliadorSelectLastOption();
-        await avaliacaoUpdatePage.questionarioSelectLastOption();
-        await avaliacaoUpdatePage.lojaSelectLastOption();
-        await waitUntilDisplayed(avaliacaoUpdatePage.getSaveButton());
-        await avaliacaoUpdatePage.save();
-        await waitUntilHidden(avaliacaoUpdatePage.getSaveButton());
-        expect(await avaliacaoUpdatePage.getSaveButton().isPresent()).to.be.false;
-
-        await avaliacaoComponentsPage.waitUntilDeleteButtonsLength(nbButtonsBeforeCreate + 1);
-        expect(await avaliacaoComponentsPage.countDeleteButtons()).to.eq(nbButtonsBeforeCreate + 1);
-    });*/
-
-  /* it('should delete last Avaliacao', async () => {
-        await avaliacaoComponentsPage.waitUntilLoaded();
-        const nbButtonsBeforeDelete = await avaliacaoComponentsPage.countDeleteButtons();
-        await avaliacaoComponentsPage.clickOnLastDeleteButton();
-
-        const deleteModal = element(by.className('modal'));
-        await waitUntilDisplayed(deleteModal);
-
-        avaliacaoDeleteDialog = new AvaliacaoDeleteDialog();
-        expect(await avaliacaoDeleteDialog.getDialogTitle().getAttribute('id')).to.match(/dcpdesconformidadesApp.avaliacao.cancel.question/);
-        await avaliacaoDeleteDialog.clickOnConfirmButton();
-
-        await avaliacaoComponentsPage.waitUntilDeleteButtonsLength(nbButtonsBeforeDelete - 1);
-        expect(await avaliacaoComponentsPage.countDeleteButtons()).to.eq(nbButtonsBeforeDelete - 1);
-    });*/
 
   after(async () => {
     await navBarPage.autoSignOut();
