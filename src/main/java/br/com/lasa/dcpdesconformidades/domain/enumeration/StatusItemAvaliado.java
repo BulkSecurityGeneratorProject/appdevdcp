27500ab9af9737d0222731afec08fd00a8312b64
Léo <leonardo.pimentel@ninebee.it>
2019-01-24 16:48:57 -0200

    Removendo código não usado.

--- src/main/java/br/com/lasa/dcpdesconformidades/domain/enumeration/StatusItemAvaliado.java
+++ src/main/java/br/com/lasa/dcpdesconformidades/domain/enumeration/StatusItemAvaliado.java
@@ -4,35 +4,20 @@ package br.com.lasa.dcpdesconformidades.domain.enumeration;
  * The StatusItemAvaliado enumeration.
  */
 public enum StatusItemAvaliado {
-    OK("OK", "Conformidade"), //
-    NAO_OK("Não OK", "Desconformidade"), //
-    N_A("N/A", "N/A");
+    OK("Conformidade"), //
+    NAO_OK("Desconformidade"), //
+    N_A("N/A");
   
-  private String descricao;
   private String classificacao;
 
-  StatusItemAvaliado(String descricao, String classificacao) {
-    this.descricao = descricao;
+  StatusItemAvaliado(String classificacao) {
     this.classificacao = classificacao;
   }
 
-  public String getDescricao() {
-    return descricao;
-  }
-  
   public String getClassificacao() {
     return classificacao;
   }
 
-  public static StatusItemAvaliado fromDescricao(String descricao) {
-    for (StatusItemAvaliado s : StatusItemAvaliado.values()) {
-      if (s.getDescricao().equals(descricao)) {
-        return s;
-      }
-    }
-    return null;
-  }
-  
   public static StatusItemAvaliado fromClassificacao(String classificacao) {
     for (StatusItemAvaliado s : StatusItemAvaliado.values()) {
       if (s.getClassificacao().equals(classificacao)) {
